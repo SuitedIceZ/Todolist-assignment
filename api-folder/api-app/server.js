90a2f0e0eabc31367aca741930baf6d904ac1ffb
SuitedIceZ <okshopm575@gmail.com>
2023-03-26 16:45:13 +0700

    [API] chore: change version of some dependencies

---
+++ api-folder/api-app/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+// Load env vars
+dotenv.config({ path: "./config/.env" });
+
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+const PORT = process.env.PORT || 5013;
+const server = app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
