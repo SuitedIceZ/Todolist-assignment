12a075a5c93990c97ddac17ac76bc6fb1db6495c
SuitedIceZ <okshopm575@gmail.com>
2023-03-26 14:46:59 +0700

    [WEB] feat: draft todo list functional

--- web-folder/web-app/src/App.js
+++ web-folder/web-app/src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [todoList, setTodoList] = useState(["task 1"]);
+  const [inputText, setText] = useState("a task");
+
+  const onChangeInputText = ({ target: { value } }) => {
+    console.log("onChangeInputText: " + value);
+    setText(value);
+  };
+
+  const addTask = () => {
+    setTodoList([...todoList, inputText]);
+
+    //clear text
+    setText("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My First React App</h1>
+      <div>
+        <input type="text" value={inputText} onChange={onChangeInputText} />
+        <button onClick={addTask}>Add</button>
+      </div>
+
+      <div>
+        <div>
+          {todoList.map((task, index) => {
+            return <div key={index}>{task}</div>;
+          })}
+        </div>
+      </div>
     </div>
   );
 }
